6a5180234d077984c56a91e387347ccd4e6ae3f0
Zira <isp009a.fedorin@yandex.ru>
2022-12-21 14:02:16 +0300

    first commit add Header

---
+++ js/main.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// SLIDER
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("myslide");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+ 
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";    
+}
+
+let timer = 0;
+makeTimer(); //Создаем интервал 
+function makeTimer(){
+    clearInterval(timer) //Очистим интервал, это позволит прервать его работу и отменить перелистывание
+    timer = setInterval(function(){
+      slideIndex++;
+      showSlides(slideIndex);
+    },5000);
+}
+
+// ===========================
